1b40dc98ee589b879fe34bdd61c92fb3576e2a02
Sarai Díaz García <dgarciasarai@gmail.com>
2018-05-07 18:07:21 +0200

    Add consumer delegates

---
+++ src/main/java/com/liferay/mobile/sdk/apio/delegates/ThingConverterDelegate.kt
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.sdk.apio.delegates
+
+import com.liferay.mobile.sdk.apio.model.Thing
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.error
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+fun <T> converter(converter: (Thing) -> T?, onConvert: (T) -> Unit): ReadWriteProperty<Any, Thing?> =
+    ThingConverterDelegate(converter, onConvert)
+
+inline fun <reified T> converter(noinline onConvert: (T) -> Unit): ReadWriteProperty<Any, Thing?> =
+    ThingConverterDelegate({ convert<T>(it) }, onConvert)
+
+@PublishedApi
+internal class ThingConverterDelegate<T>(
+    val converter: (Thing) -> T?,
+    val onConvert: (T) -> Unit) : ReadWriteProperty<Any, Thing?> {
+
+    private var thing: Thing? by observeNonNull { it.let(converter)?.apply(onConvert) }
+
+    override fun getValue(thisRef: Any, property: KProperty<*>): Thing? = thing
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: Thing?) {
+        thing = value
+    }
+
+}
+
+inline fun <reified T> convert(thing: Thing): T? = convert(T::class.java, thing)
+
+fun <T> convert(clazz: Class<T>, thing: Thing): T? {
+    val t = (converters[clazz.name] as? (Thing) -> T)?.invoke(thing)
+
+    if (t == null) {
+        AnkoLogger(clazz).error { "Converter not found for this class" }
+    }
+
+    return t
+}
+
+var converters: Map<String, (Thing) -> Any> = emptyMap()
